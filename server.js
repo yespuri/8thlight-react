21c647224f0631e25ac51b11d414c89edc6c8120
BQ <qian.brian@gmail.com>
2019-01-18 08:44:29 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const axios = require('axios');
+require('dotenv').load();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get('/api/search/:keyword', (req, res) => {
+  console.log('get request backend');
+  const apiKey = process.env.APIkey;
+  const keyword = req.params.keyword;
+  console.log(keyword);
+  axios
+    .get(`https://www.googleapis.com/books/v1/volumes?q=${keyword}&key=${apiKey}`)
+    .then(resp => res.send(resp.data));
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
