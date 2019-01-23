2cab839c29af35a1aec4f39ed19d0d64b8bd4a15
BQ <qian.brian@gmail.com>
2019-01-23 12:38:18 -0800

    production testing

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ require('dotenv').load();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static(__dirname + '/public'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('/build'));
+} else {
+  app.use(express.static(__dirname + '/public'));
+}
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
