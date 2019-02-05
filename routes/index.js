16d3280b389de24af92cc217acc6c90ea68dc8ef
BQ <qian.brian@gmail.com>
2019-02-05 11:36:43 -0800

    testing

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.use('/api', apiRoutes);
 if (process.env.NODE_ENV === 'production') {
   router.use(express.static('client/build'));
   router.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join('client/build', 'index.html'));
   });
 } else {
   router.use(express.static(__dirname + 'client/public'));
