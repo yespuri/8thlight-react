50181dd64470b85574b16fbaa5402d01c3d2d52e
BQ <qian.brian@gmail.com>
2019-02-05 01:24:49 -0800

    dotenv testing, reordered routing files in server.js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const routes = require('./routes');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
-
-const app = express();
-const PORT = process.env.PORT || 3001;
 
 app.use(routes);
 
